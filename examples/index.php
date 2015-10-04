65e629d1c976452c79a84207df86a07a02867f69
xkid <chchin83@gmail.com>
2015-10-04 22:41:58 +0800

    Make index page for examples
    List all page in examples directory, but exclude . .. index.php user and
    all .html

---
+++ examples/index.php
@@ -0,0 +1,38 @@
+<?php
+
+  // Use this PHP File As your Development
+  
+	// ****************  Default include  ****************
+	
+		// include All file
+		include('../all.php');
+		
+		// New template engine object
+		$TBS =& new clsTinyButStrong;
+		// Load Your HTML file
+		$TBS->LoadTemplate('index.html');    // <----- Place this script HTML
+	
+	// ****************************************************
+	
+	// **************  Write Your Code Here  **************
+	
+	$title = 'Rojak Engine Examples';
+	
+	// Scan All files in current directory
+	$links = scandir('./');
+	// Check each link for filter . .. index.php user and all .html
+	foreach ($links as $key => $link) {
+		if ((substr($link,-5) == '.html') || ($link == '.') || ($link == '..') || ($link == 'index.php') || ($link == 'user')){
+			// Remove that array
+			unset($links[$key]);
+		}
+	}
+	// Merge array to HTML
+	$TBS->MergeBlock('blk1',$links);
+	
+	// ****************************************************
+	
+	// Show result
+	$TBS->Show() ;
+	
+?>
